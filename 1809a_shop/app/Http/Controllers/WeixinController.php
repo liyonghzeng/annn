a01bb830b365d8832dc8de63698fe5f266679eca
root <2548722906@qq.com>
2019-04-10 19:10:18 +0800

    m微信回复

--- 1809a_shop/app/Http/Controllers/WeixinController.php
+++ 1809a_shop/app/Http/Controllers/WeixinController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Redis;
 
+use \App\Wx;
+
 use DB;
 
 class WeixinController extends Controller
@@ -18,16 +20,40 @@ class WeixinController extends Controller
 
     public function index(){
         $content=file_get_contents("php://input");
-        $data=simplexml_load_string($content);
-//        echo 'ToUserName'.$data->ToUserName;
-        echo  $data->ToUserName;
-        echo  $CreateTime = $data->CreateTime;
-        echo  $Event = $data->Event;
-        echo  $data->EventKey;
-
         $time=date('Y-m-d H:i:s',time());
         $str=$time.$content."\n";
-        file_put_contents("logs/wx_event.log",$str,FILE_APPEND);
+        file_put_contents("logs/wx_event.log",$str,FILE_APPEND);    
+        $data=simplexml_load_string($content);
+//        echo 'ToUserName'.$data->ToUserName;
+    //   echo 'ToUserName: '. $data->ToUserName;echo '</br>';        // 公众号ID
+    //   echo 'FromUserName: '. $data->FromUserName;echo '</br>';    // 用户OpenID
+    //   echo 'CreateTime: '. $data->CreateTime;echo '</br>';        // 时间戳
+    //   echo 'MsgType: '. $data->MsgType;echo '</br>';              // 消息类型
+    //   echo 'Event: '. $data->Event;echo '</br>';                  // 事件类型
+    //   echo 'EventKey: '. $data->EventKey;echo '</br>';
+        $wx_id = $data->ToUserName;             // 公众号ID
+        $openid = $data->FromUserName;          //用户OpenID
+        $event = $data->Event;          //事件类型
+        if($event=='subscribe'){    //扫码事件
+            $local_user = Wx::where(['openid'=>$openid])->first();
+            if($local_user){        //用户之前关注过
+                echo '<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$wx_id.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['. '欢迎回来 '. $local_user['nickname'] .']]></Content></xml>';
+            }else{          //用户首次关注
+                //获取用户信息
+                $u = $this->getUserInfo($openid);
+                //用户信息入库
+                $u_info = [
+                    'openid'    => $u['openid'],
+                    'nickname'  => $u['nickname'],
+                    'sex'  => $u['sex'],
+                    'headimgurl'  => $u['headimgurl'],
+                ];
+                $id = WxUserModel::insertGetId($u_info);
+                echo '<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$wx_id.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['. '欢迎关注 '. $u['nickname'] .']]></Content></xml>';
+            }
+        }
+    
+       
         echo 'SUCCESS';
     }
     /**获取微信 AccessToren */
---
+++ 1809a_shop/app/Wx.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Wx extends Model
+{
+    //
+    protected $table = 'wx';
+    public $timestamps = false;
+}
